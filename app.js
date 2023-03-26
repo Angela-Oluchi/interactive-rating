e994fce7027b6b60f03fcf8cb1eb3b5f01146b8c
Angela Ria <angelaabugu01@gmail.com>
2023-03-26 22:39:28 +0100

    zweite

---
+++ app.js
@@ -0,0 +1,15 @@
+function validbtn () {
+    alert("hello")
+    // let btnone = document.btnclick.one;
+    // let btntwo = document.btnclick.two;
+    // let btnthree = document.btnclick.three;
+    // let btnfour = document.btnclick.four;
+    // let btnfive = document.btnclick.five;
+    // let successMessage = document.getElementById("thanks");
+     
+}
+
+function validonebtn() {
+    
+    
+}
